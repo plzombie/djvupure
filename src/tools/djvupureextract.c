ed5bad863e00a0a461dd4bf90f6d6759f00392c0
Mikhail Morozov <plzombie@yandex.ru>
2023-08-05 23:31:24 +0300

    Add djvupureextract utility

--- src/tools/djvupureextract.c
+++ src/tools/djvupureextract.c
@@ -26,12 +26,13 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 #include "../../include/djvupure.h"
-#include "aux_insert.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <wchar.h>
 #include <locale.h>
+
+bool ExtractChunkFromPage(djvupure_chunk_t *page, const uint8_t sign[4], wchar_t *param);
 
 int wmain(int argc, wchar_t **argv)
 {
@@ -52,13 +53,8 @@ int wmain(int argc, wchar_t **argv)
 		if(_command) command = _command+1;
 
 		wprintf(L"%ls document.djvu CHUNK1=param1 CHUNK2=param2 ...\n"
-			L"\tfor INFO chunk there are special parameters \"INFO=width,height,dpi,rotation,gamma\", some parameters can be empty\n"
-			L"\t\trotation 1 is 0deg, 5 - 90deg, 2 - 180deg, 6 - 270deg\n"
-			L"\t\tgamma 22 stands for gamma value 2.2\n"
-			L"\tfor other chunks parameter is a path to a file containing chunk data (i.e. \"Sjbz=page.sjbz\")\n"
-			L"\tChunks FG44 and BG44 should be a IFF85 file with a group of PM44 subchunks (can be created with extract utility)\n"
-			L"\t\tchunk FG44 extracts only one PM44 chunk from file\n"
-			L"\t\tchunk BG44 can be defined like \"BG44=file.bg44,n\" where n is a number of chunks to copy\n",
+			L"\tParameter is a path to a file containing chunk data (i.e. \"Sjbz=page.sjbz\")\n"
+			L"\tFor chunks FG44 and BG44 an IFF85 file with a group of PM44 subchunks will be produced\n",
 			command);
 
 		return EXIT_SUCCESS;
@@ -97,14 +93,9 @@ int wmain(int argc, wchar_t **argv)
 
 		chunk_filename = argv[i]+j+1;
 		
-		if(!InsertChunkToPage(page, sign, chunk_filename))
-			wprintf(L"Can't append chunk %.4hs\n", sign);
+		if(!ExtractChunkFromPage(page, sign, chunk_filename))
+			wprintf(L"Can't extract chunk %.4hs\n", sign);
 	}
-
-	fctx = djvupureFileOpenW(argv[1], true);
-	if(!fctx) goto FINAL;
-
-	if(!djvupureDocumentRender(document, &io, fctx)) goto FINAL;
 	
 	result = EXIT_SUCCESS;
 	
@@ -113,4 +104,105 @@ FINAL:
 	if(document) djvupureChunkFree(document);
 	
 	return result;
+}
+
+bool ExtractIW44ChunkFromPage(djvupure_chunk_t *page, const uint8_t sign[4], wchar_t *param)
+{
+	const uint8_t pm44_sign[4] = { 'P', 'M', '4', '4' };
+	djvupure_io_callback_t io;
+	void *fctx = 0;
+	djvupure_chunk_t *chunk;
+	size_t nof_subchunks;
+
+	nof_subchunks = djvupureContainerCountSubchunksBySign(page, sign, 0);
+	if(!nof_subchunks) return false;
+
+	chunk = djvupureContainerCreate(pm44_sign);
+
+	for(size_t index = 0; index < nof_subchunks; index++) {
+		djvupure_chunk_t *old_subchunk, *new_subchunk;
+		void *data;
+		size_t data_len;
+
+		old_subchunk = djvupureContainerGetSubchunkBySign(page, sign, 0, index);
+		if(!old_subchunk) {
+			djvupureChunkFree(chunk);
+
+			return false;
+		}
+
+		djvupureRawChunkGetDataPointer(old_subchunk, &data, &data_len);
+		if(!data || !data_len) continue;
+
+		new_subchunk = djvupureRawChunkCreate(pm44_sign, data, data_len);
+		if(!new_subchunk) {
+			djvupureChunkFree(chunk);
+
+			return false;
+		}
+
+		if(!djvupureContainerInsertChunk(chunk, new_subchunk, djvupureContainerSize(chunk))) {
+			djvupureChunkFree(chunk);
+			djvupureChunkFree(new_subchunk);
+
+			return false;
+		}
+	}
+
+	djvupureFileSetIoCallbacks(&io);
+
+	fctx = djvupureFileOpenW(param, true);
+	if(!fctx) {
+		djvupureChunkFree(chunk);
+
+		return false;
+	}
+
+	if(!djvupureChunkRender(chunk, &io, fctx)) {
+		djvupureChunkFree(chunk);
+		djvupureFileClose(fctx);
+
+		return false;
+	}
+
+	djvupureChunkFree(chunk);
+	djvupureFileClose(fctx);
+
+	return true;
+}
+
+bool ExtractChunkFromPage(djvupure_chunk_t *page, const uint8_t sign[4], wchar_t *param)
+{
+
+	if(!memcmp(sign, "FG44", 4) || !memcmp(sign, "BG44", 4)) { // Process FG44 or BG44 chunk
+		return ExtractIW44ChunkFromPage(page, sign, param);
+	} else { // Process others
+		djvupure_io_callback_t io;
+		void *fctx = 0;
+		djvupure_chunk_t *chunk;
+		void *data;
+		size_t data_len;
+
+		chunk = djvupureContainerGetSubchunkBySign(page, sign, 0, 0);
+		if(!chunk) return false;
+
+		djvupureRawChunkGetDataPointer(chunk, &data, &data_len);
+
+		if(!data || !data_len) return false;
+	
+		djvupureFileSetIoCallbacks(&io);
+
+		fctx = djvupureFileOpenW(param, true);
+		if(!fctx) return false;
+
+		if(io.callback_write(fctx, data, data_len) != data_len) {
+			djvupureFileClose(fctx);
+
+			return false;
+		}
+
+		djvupureFileClose(fctx);
+	}
+
+	return true;
 }
